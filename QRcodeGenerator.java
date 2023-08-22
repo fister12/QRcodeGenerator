0551881fb5d1f2180178d4dd75a399d745c74282
@aadik04 <66963537+fister12@users.noreply.github.com>
2023-08-22 19:27:53 +0530

    Update QRcodeGenerator.java

--- QRcodeGenerator.java
+++ QRcodeGenerator.java
@@ -1,47 +1,35 @@
-import java.awt.Color;
-import java.awt.Graphics2D;
-import java.awt.image.BufferedImage;
+import net.glxn.qrgen.QRCode;
+import net.glxn.qrgen.image.ImageType;
+
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import javax.imageio.ImageIO;
-import com.google.zxing.WriterException;
-import com.google.zxing.common.BitMatrix;
-import com.google.zxing.qrcode.QRCodeWriter;
 
-public class QRcodeGenerator {
-    public static void generateQRCodeImage(String text, int width, int height, String filePath)
-            throws WriterException, IOException {
-        QRCodeWriter qrCodeWriter = new QRCodeWriter();
-        BitMatrix bitMatrix = qrCodeWriter.encode(text, com.google.zxing.BarcodeFormat.QR_CODE, width, height);
-
-        BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-        bufferedImage.createGraphics();
-
-        Graphics2D graphics = (Graphics2D) bufferedImage.getGraphics();
-        graphics.setColor(Color.WHITE);
-        graphics.fillRect(0, 0, width, height);
-        graphics.setColor(Color.BLACK);
-
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
-                if (bitMatrix.get(i, j)) {
-                    graphics.fillRect(i, j, 1, 1);
-                }
-            }
-        }
-        ImageIO.write(bufferedImage, "png", new File(filePath));
+public class QRCodeGenerator {
+    public static void main(String[] args) {
+        String data = "https://www.example.com";
+        generateQRCode(data, "qrcode_example.png");
     }
 
-    public static void main(String[] args) {
+    public static void generateQRCode(String data, String filePath) {
+        ByteArrayOutputStream out = QRCode.from(data)
+                .withSize(250, 250)
+                .to(ImageType.PNG)
+                .stream();
+
         try {
-            generateQRCodeImage("https://www.example.com", 350, 350, "QRCode.png");
-        } catch (WriterException e) {
-            System.out.println("Could not generate QR Code, WriterException :: " + e.getMessage());
+            FileOutputStream fos = new FileOutputStream(new File(filePath));
+            fos.write(out.toByteArray());
+            fos.flush();
+            fos.close();
+            System.out.println("QR code saved to: " + filePath);
         } catch (IOException e) {
-            System.out.println("Could not generate QR Code, IOException :: " + e.getMessage());
+            e.printStackTrace();
         }
     }
 }
+
 //Author @Aaditya kapruwan
 //Text field of string object form is text is the text to be encoded while width and height specify the size of the image produced
 //We convert the text into a bit matrix and this bitmatrix is subsequently encoded ushing graphics2D into a white background as black spots
